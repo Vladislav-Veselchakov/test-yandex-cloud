66294acc12baf05b44e396b6b55dc4009661d84f
Vladislav A <validator1@yandex.ru>
2023-05-25 19:53:50 +0300

    Initial commit

---
+++ src/main/java/yandCloud/TestYandexCloudApplication.java
@@ -0,0 +1,13 @@
+package yandCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestYandexCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestYandexCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yandCloud/TestYandexCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package yandCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestYandexCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
