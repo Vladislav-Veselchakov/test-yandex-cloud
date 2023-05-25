589f166799213d4a9c1745e327a64ba0d5b80ee2
Vladislav A <validator1@yandex.ru>
2023-05-25 21:07:47 +0300

    добавили thymeleaf в зависимости для видимости страниц

--- src/main/java/yandCloud/RestController.java
+++ src/main/java/yandCloud/RestController.java
@@ -1,13 +1,14 @@
 package yandCloud;
 
 import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestControllerж
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-    @GetMapping("/")
-    public String mainPage() {
-
-        return "Hello vl";
-    }
+//    @GetMapping("/")
+//    public String mainPage() {
+//
+//        return "Hello vl";
+//    }
 }
--- src/main/java/yandCloud/mainController.java
+++ src/main/java/yandCloud/mainController.java
@@ -2,12 +2,11 @@ package yandCloud;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class mainController {
-//    @GetMapping("/")
-//    public String mainPage() {
-//        return "mainPage";
-//    }
+    @GetMapping("/")
+    public String mainPage() {
+        return "mainPage";
+    }
 }
